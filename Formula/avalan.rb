70ebb7961a3aa2a16c4ca00376183540743aca42
Mariano Iglesias <mariano@cricava.com>
2026-05-01 14:37:14 -0300

    Add brew CI workflow and install formula resources first

--- Formula/avalan.rb
+++ Formula/avalan.rb
@@ -16,6 +16,7 @@ class Avalan < Formula
 
   def install
     venv = virtualenv_create(libexec, "python3.12")
+    venv.pip_install resources
     venv.pip_install_and_link resource("avalan")
   end
 
